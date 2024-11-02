82945764454ff0d3d3ca9bb76cc952a246e7ee5e
Muhammad Waqas <waqas4346@gmail.com>
2024-11-02 21:06:19 +0500

    changes after running commands to create function for discount

---
+++ extensions/product-discount/src/run.js
@@ -0,0 +1,56 @@
+// @ts-check
+import { DiscountApplicationStrategy } from "../generated/api";
+
+// Use JSDoc annotations for type safety
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ * @typedef {import("../generated/api").Target} Target
+ * @typedef {import("../generated/api").ProductVariant} ProductVariant
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+const EMPTY_DISCOUNT = {
+  discountApplicationStrategy: DiscountApplicationStrategy.First,
+  discounts: [],
+};
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  const targets = input.cart.lines
+    // Only include cart lines with a quantity of two or more
+    .filter((line) => line.quantity >= 2)
+    .map((line) => {
+      return /** @type {Target} */ ({
+        // Use the cart line ID to create a discount target
+        cartLine: {
+          id: line.id,
+        },
+      });
+    });
+  if (!targets.length) {
+    // You can use STDERR for debug logs in your function
+    console.error("No cart lines qualify for volume discount.");
+    return EMPTY_DISCOUNT;
+  }
+
+  return {
+    discounts: [
+      {
+        // Apply the discount to the collected targets
+        targets,
+        // Define a percentage-based discount
+        value: {
+          percentage: {
+            value: "10.0",
+          },
+        },
+      },
+    ],
+    discountApplicationStrategy: DiscountApplicationStrategy.First,
+  };
+}
